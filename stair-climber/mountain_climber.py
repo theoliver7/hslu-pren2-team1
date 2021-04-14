6a6cc6e146a153879605d4e3299e0f76c8993a36
Jan Kalbermatter <jan.kalbermatter@gmail.com>
2021-04-14 19:32:14 +0200

    first try other functionality

--- stair-climber/mountain_climber.py
+++ stair-climber/mountain_climber.py
@@ -16,20 +16,20 @@ class MountainClimber:
     videoStream = VideoStream()
     objectDetector = ObjectDetector()
     detectedPictogram = None
-    
+
     def wait_for_start(self):
         self.buttons.wait_for_startbutton()
 
     def search_identify_pictogram(self):
-        # 1 - Start Video Stream
+        # Start Video Stream
         videoStream.start()
 
-        # 2 - Look for Object for x seconds
+        # Look for Object for x seconds
         rotationDegrees = 0
         while detectedPictogram is None: 
             detectedPictogram = objectDetector.analyzeVideo(videoStream, 5)
 
-            # 3 - If no Object found, turn by x Degrees. If Object found, return to starting Position
+            # If no Object found, turn by x Degrees. If Object found, return to starting Position
             if detectedPictogram is None: 
                 rotationDegrees += 30
                 rotation.go_to_degree(rotationDegrees)
@@ -41,10 +41,31 @@ class MountainClimber:
 
 
     def drive_to_stairs(self):
-        raise NotImplementedError
+        # are we able to just drive straight or does the robot have to dodge the flag or alter his path otherwise
+        # is this possible with the UltraSonic sensors?
+
+        # drive forward
+        frontMotor.accelerate() # can i set the speed somewhere? 
+        distance_threshhold = 15 # idk what to put here, what value does get_distance give me?
+
+        # noop while robot is far from stairs
+        while ultra_sonic.get_distance1() > distance_threshhold:
+            pass
+
+        # Closer than threshhold
+        frontMotor.stop()
+
+        # maybe drive a little further to make sure we are touching the stairs?
 
     def climb_stairs(self):
-        raise NotImplementedError
+        numberOfStairs = 5 
+
+        for i in range(numberOfStairs + 1):
+            self.__climb_one_stair()
 
     def __climb_one_stair(self):
-        raise NotImplementedError
+        # Check if path is blocked
+
+        # Move to side 
+
+        
